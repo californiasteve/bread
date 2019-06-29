89a1838e65de7852c7e540944f8d572581f7e710
Steve Scranton <31661648+californiasteve@users.noreply.github.com>
2019-06-28 21:28:04 -0500

    Updated line 980
    added "it"

--- bmlt-meeting-list.php
+++ bmlt-meeting-list.php
@@ -977,7 +977,7 @@ if (!class_exists("Bread")) {
 						
 						if ( $this->options['weekday_language'] === 'fr' ) {
 							$cont = '(suite)';							
-						} else if ( $this->options['weekday_language'] === 'se' || $this->options['weekday_language'] === 'dk' ) {
+						} else if ( $this->options['weekday_language'] === 'se' || $this->options['weekday_language'] === 'dk' || $this->options['weekday_language'] === 'it' ) {
                             $cont = '(forts)';
                         } else {
 							$cont = '(cont)';
